9176577a09e552183cbe271f08ef5d93ee17c3f2
Your Name <you@example.com>
2019-08-03 11:34:14 +0600

    first commit

---
+++ src/GoodDog.java
@@ -0,0 +1,21 @@
+public class GoodDog {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Gav, gav!");
+        } else if (size > 14) {
+            System.out.println("Vuf, vuf!");
+        } else {
+            System.out.println("Taf, taf");
+        }
+    }
+}
---
+++ src/GoodDogTestDrive.java
@@ -0,0 +1,13 @@
+public class GoodDogTestDrive {
+
+    public static void main(String[] args) {
+        GoodDog one = new GoodDog();
+        one.setSize(16);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+        System.out.println("First dog: " + one.getSize());
+        System.out.println("Second dog: " + two.getSize());
+        one.bark();
+        two.bark();
+    }
+}
